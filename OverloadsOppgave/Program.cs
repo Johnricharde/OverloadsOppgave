be763dddf1a81418be725b8d50f73eb94603226a
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-16 10:29:53 +0100

    Add project files.

---
+++ OverloadsOppgave/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OverloadsOppgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintWelcomeMessage();
+        }
+
+        static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Hei og velkommen");
+        }
+        static void PrintWelcomeMessage(string name = "Terje")
+        {
+            Console.WriteLine($"Hei og velkommen {name}");
+        }
+    }
+}
